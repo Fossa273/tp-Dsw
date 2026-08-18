9f4093ef81d098a40e3953a65ef14dfec5931f64
Fossa273 <fossatinico96@gmail.com>
2026-08-17 23:16:54 -0300

    CRUD Localidades + Provincias Done, falta cambiar clientes y vehiculos (error)

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,4 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 import { router as clienteRoutes } from './cliente/cliente.routes.js';
 import { router as localidadRoutes } from './localidad/localidad.routes.js';
 import { router as provinciaRoutes } from './provincia/provincia.routes.js';
@@ -6,6 +7,7 @@ import { router as vehiculoRoutes } from './vehiculo/vehiculo.routes.js';
 
 const app = express();
 const PORT = 3000;
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/clientes', clienteRoutes);
